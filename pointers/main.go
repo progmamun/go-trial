12c8803b2b38ee42c22b04d460d1fd70fd8cb9b8
progmamun <mamun333089@gmail.com>
2021-10-01 22:42:45 +0600

    go mod

---
+++ pointers/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("welcome to pointers class")
+
+	// var ptr *int
+	//*string
+
+	// fmt.Println("value of pointer is ", ptr)
+
+	myNumber := 23
+
+	var ptr = &myNumber
+	//ref. mean &
+
+	fmt.Println("value of actual pointer is ", ptr)
+	fmt.Println("value of actual pointer is ", *ptr)
+
+	*ptr = *ptr * 2
+	fmt.Println("New value is: ", myNumber)
+}
