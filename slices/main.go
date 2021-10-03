0b23435bb09bfe7f1e29696d37144f97922a3682
progmamun <mamun333089@gmail.com>
2021-10-03 22:40:31 +0600

    slices advance

--- slices/main.go
+++ slices/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
 	fmt.Println("welcome to slices lecture")
@@ -14,4 +17,19 @@ func main() {
 	fruitList = append(fruitList[1:])
 	//[1:3] [:3]
 	fmt.Println(fruitList)
+
+	highScores := make([]int, 4)
+
+	highScores[0] = 234
+	highScores[1] = 945
+	highScores[2] = 465
+	highScores[3] = 867
+	// highScores[1] = 777
+
+	highScores = append(highScores, 555, 666, 321)
+	fmt.Println(highScores)
+
+	sort.Ints(highScores)
+	fmt.Println(highScores)
+	fmt.Println(sort.IntsAreSorted(highScores))
 }
