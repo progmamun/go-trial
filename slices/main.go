aa5d47d1f9ac43e355ff5ceb1983f350fd93a554
progmamun <mamun333089@gmail.com>
2021-10-03 22:51:49 +0600

    slices advance2

--- slices/main.go
+++ slices/main.go
@@ -32,4 +32,12 @@ func main() {
 	sort.Ints(highScores)
 	fmt.Println(highScores)
 	fmt.Println(sort.IntsAreSorted(highScores))
+
+	//how to remove a value from slices based on index
+
+	var courses = []string{"reactjs", "javascript", "swift", "python", "ruby"}
+	fmt.Println(courses)
+	var index int = 2
+	courses = append(courses[:index], courses[index+1:]...)
+	fmt.Println(courses)
 }
