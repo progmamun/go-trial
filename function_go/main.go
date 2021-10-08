cc887cc7cf6ef32abb895126202c354a5e95f1a5
progmamun <mamun333089@gmail.com>
2021-10-08 08:43:22 +0600

    function in golnag

---
+++ function_go/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to function in golang")
+	// greeter()
+
+	result := adder(3, 5)
+
+	fmt.Println("Result is: ", result)
+
+	proRes, myMessage := proAdder(2, 5, 8, 7, 3)
+	fmt.Println("Pro result is: ", proRes)
+	fmt.Println("Pro Message is: ", myMessage)
+
+}
+
+func adder(valOne int, ValTwo int) int {
+	return valOne + valTwo
+}
+
+func proAdder(values ...int) (int, string) {
+	total := 0
+
+	for _, val := range values{
+		total += val
+	}
+	
+	return total, "Hi Pro result function"
+
+	// func greeter() {
+// 	fmt.Println("Assalamualikum from golang")
+// }
