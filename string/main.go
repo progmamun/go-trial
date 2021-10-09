57c7e8b83871609216e83c284d0138937249108d
progmamun <mamun333089@gmail.com>
2021-10-09 15:39:14 +0600

    string in golnag

---
+++ string/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	//strings writes in double "" couts
+	var nameOne string = "Mango"
+	var nameTwo = "luigi"
+	var nameThree string
+	fmt.Println(nameOne, nameTwo, nameThree)
+}
+
+// note: short hand variables name cannot use outside of function (:=)
